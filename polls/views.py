24084e14868e8764e0f51185afda2fe98784c1ad
Chaserfrank <wangangafrancis063@gmail.com>
2026-01-01 14:47:00 +0300

    Added test cases

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,10 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    #output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    try:
-        question = get_object_or_404(Question, pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
